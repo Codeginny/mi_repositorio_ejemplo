05c0b3681b4c2314af125617618cf9964186074a
Codeginny <viir.ponce@gmail.com>
2024-10-16 20:20:24 -0300

    Archivo node.js

---
+++ Hello.js
@@ -0,0 +1,5 @@
+// El siguiente código imprime "Hola, Mundo" y un mensaje adicional
+
+console.log("Hola, Mundo");
+console.log("Este es un programa simple en Node.js.");
+console.log("¡Espero que tengas un gran día!");
